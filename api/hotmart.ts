553a18cb3fc0c1d30fe9387370fbd5eeacd546e1
LucasDomin <145492050+LucasDomin@users.noreply.github.com>
2026-05-01 13:04:15 -0300

    update

--- api/hotmart.ts
+++ api/hotmart.ts
@@ -20,7 +20,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
         "Content-Type": "application/json",
         "hottok": "457bf8d4-f6ef-4eaf-91d8-9934757e908f",
       },
-      body: JSON.stringify(req.body),
+      body: JSON.stringify({
+        ...req.body,
+        tags: ["LM-CALCULADORA-LAUDO-MASTER"],
+      }),
     });
 
     const status = response.status;
