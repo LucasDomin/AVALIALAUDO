8aff8776014f3aaa635e36d6d1b30e96c9fcf9e5
LucasDomin <145492050+LucasDomin@users.noreply.github.com>
2026-05-04 20:41:07 -0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 import App from "./App";
 
 // Substitua pelo seu CLIENT_ID do Google Cloud Console
-const GOOGLE_CLIENT_ID = "SEU_CLIENT_ID_AQUI.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = "172388166776-4rj6oe6npprn3k3e4ftj2k2t3h8fpu6j.apps.googleusercontent.comm";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
